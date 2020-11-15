92374ac3f528d13ef59a9c537d32ffc37a1fbc80
Hoang Nga Nguyen <ngasoft@gmail.com>
2020-11-15 12:26:26 +0000

    first commit

---
+++ ReadingRuler/MainWindow.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace ReadingRuler
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.GlassPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // GlassPanel
+            // 
+            this.GlassPanel.BackColor = System.Drawing.Color.Maroon;
+            this.GlassPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.GlassPanel.Location = new System.Drawing.Point(12, 147);
+            this.GlassPanel.Name = "GlassPanel";
+            this.GlassPanel.Size = new System.Drawing.Size(776, 67);
+            this.GlassPanel.TabIndex = 0;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GlassPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.Opacity = 0.8D;
+            this.Text = "Reading Ruler";
+            this.TransparencyKey = System.Drawing.Color.Maroon;
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainWindow_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainWindow_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MainWindow_MouseUp);
+            this.Move += new System.EventHandler(this.MainWindow_Resize);
+            this.Resize += new System.EventHandler(this.MainWindow_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel GlassPanel;
+    }
+}
+
---
+++ ReadingRuler/MainWindow.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ReadingRuler
+{
+
+
+    public partial class MainWindow : Form
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        int padding = 10;
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            GlassPanel.Left = padding;
+            GlassPanel.Width = Width - 4 * padding;
+        }
+ 
+        private void MainWindow_Resize(object sender, EventArgs e)
+        {
+            GlassPanel.Left = padding;
+            GlassPanel.Width = Width - 4 * padding;
+        }
+
+        Boolean isResizingTop = false;
+        Boolean isResizingBot = false;
+        int lastX = 0;
+        int lastY = 0;
+        private void MainWindow_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastX = e.X;
+            lastY = e.Y;
+            if (GlassPanel.Top - lastY <= 20 && GlassPanel.Top - lastY >= 0)
+            {
+                isResizingTop = true;
+            }
+            else if (lastY - (GlassPanel.Top + GlassPanel.Height) <= 20 && lastY - (GlassPanel.Top + GlassPanel.Height) >= 0)
+            {
+                isResizingBot = true;
+            }
+            else
+            {
+                isResizingTop = true;
+                isResizingBot = true;
+            }
+        }
+
+
+
+        private void MainWindow_MouseUp(object sender, MouseEventArgs e)
+        {
+            isResizingTop = false;
+            isResizingBot = false;
+        }
+
+        private void MainWindow_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isResizingTop || isResizingBot)
+            {
+                int delta = e.Y - lastY;
+                lastX = e.X;
+                lastY = e.Y;
+                if (isResizingTop) GlassPanel.Top += delta;
+                if (!isResizingBot)
+                {
+                    GlassPanel.Height -= delta;
+                }
+            }
+            
+        }
+
+
+    }
+}
